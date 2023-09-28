8c3f56964406306e452765d1a8696d0358d4fc4d
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2023-09-28 23:11:35 +0300

    Task 2

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,26 +2,71 @@ document.addEventListener("DOMContentLoaded", function () {
     const inputField = document.getElementById("inputField");
     const output = document.getElementById("output");
 
-    let timeoutId; // To store the timer ID
+    let timeoutId;
 
-    // Function to reverse a string
     function reverseString(str) {
         return str.split("").reverse().join("");
     }
 
-    // Function to update the output
     function updateOutput() {
         const inputValue = inputField.value;
         const reversedValue = reverseString(inputValue);
         output.textContent = `Reversed: ${reversedValue}`;
     }
 
-    // Event listener for input changes
     inputField.addEventListener("input", function () {
-        // Clear the previous timer, if any
         clearTimeout(timeoutId);
 
-        // Start a new timer
-        timeoutId = setTimeout(updateOutput, 1000); // 1000 milliseconds (1 second) delay
+        timeoutId = setTimeout(updateOutput, 1000);
     });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const dateButtonsContainer = document.querySelector(".date-buttons");
+    const imageElement = document.getElementById("image");
+    const descriptionElement = document.getElementById("description");
+    const errorMessageElement = document.getElementById("errorMessage");
+    const completionMessageElement = document.getElementById("completionMessage");
+
+    for (let day = 1; day <= 30; day++) {
+        const button = document.createElement("button");
+        button.textContent = `Sep ${day}`;
+        button.dataset.day = day;
+        dateButtonsContainer.appendChild(button);
+
+        button.addEventListener("click", () => {
+            const selectedDay = button.dataset.day;
+
+            imageElement.src = "";
+            descriptionElement.textContent = "";
+            errorMessageElement.textContent = "";
+            completionMessageElement.textContent = "";
+
+            const apiKey = "4iZ6ktLmS5fntPwpbmhoyN2fH1JmkgOjQaY7Z0mK";
+            const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=2023-09-${selectedDay}`;
+
+            fetch(apiUrl)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error("Network response was not ok");
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    if (data.media_type === "image") {
+                        imageElement.src = data.hdurl;
+                    } else if (data.media_type === "video") {
+                        alert("This date has a video, not an image.");
+                        imageElement.src = "";
+                    }
+                    descriptionElement.textContent = data.explanation;
+                })
+                .catch((error) => {
+                    errorMessageElement.textContent = `Error: future data`;
+                })
+                .finally(() => {
+                    completionMessageElement.textContent = "Request completed.";
+                });
+        });
+    }
 });
